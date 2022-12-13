646ba44fb3ceb732c40cd68d2489a80c88ca0697
Manan Patel <mananpatel542@gmail.com>
2022-12-13 23:42:52 +0530

    main model and fetched tesla stock data

--- predict_stock.py
+++ predict_stock.py
@@ -35,3 +35,73 @@ def dataConvert(data):
     x_data = np.array(x_data)
     y_data = np.array(y_data)
     return x_data, y_data
+
+
+# collecting data set
+start = datetime.datetime(2010, 1, 1)
+end = datetime.datetime.now()
+stocks = ["KRX:005380", "NASDAQ:AAPL", "NASDAQ:GOOGL", "KRX:005930"]
+
+print(len)
+df = []
+for i, stock in enumerate(stocks):
+    df.append(data.DataReader(
+        stock, "google", start, end
+    ))
+df[0].as_matrix()
+data = np.loadtxt('TSLA.csv', delimiter=',')
+x_data, y_data = dataConvert(data)
+
+
+print(y_data.shape)
+train_size = int(len(y_data) * 0.7)
+x
+x_train = np.array(x_data[:train_size])
+x_test = np.array(x_data[train_size:])
+
+y_train = np.array(y_data[:train_size])
+y_test = np.array(y_data[train_size:])
+
+print('tx:', x_train.shape, 'tex:', x_test.shape,
+      'ty:', y_train.shape, 'tey:', y_test.shape)
+
+X = tf.placeholder(tf.float32, [None, None, inputD])
+Y = tf.placeholder(tf.float32, [None, outD])
+
+cell = rnn.BasicLSTMCell(num_units=10, state_is_tuple=True, activation=tf.tanh)
+
+outputs, _state = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
+y_pred = tf.contrib.layers.fully_connected(outputs[:, -1], outD)
+
+loss = tf.reduce_sum(tf.square(y_pred - Y))
+
+optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+train = optimizer.minimize(loss)
+
+
+targets = tf.placeholder(tf.float32, [None, 1])
+predictions = tf.placeholder(tf.float32, [None, 1])
+rmse = tf.sqrt(tf.reduce_mean(tf.square(targets - predictions)))
+
+otherData = np.loadtxt('data-02-stock_daily.csv', delimiter=',')
+otherX, otherY = dataConvert(otherData)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(1001):
+        _, step_loss = sess.run([train, loss], feed_dict={
+                                X: x_train, Y: y_train})
+        if(i % 100 == 0):
+            print(i, step_loss)
+
+    result = sess.run(y_pred, feed_dict={X: otherX})
+    rmse = sess.run(rmse, feed_dict={targets: result, predictions: otherY})
+
+
+print("rmse:", rmse)
+
+plt.plot(otherY, 'red')
+plt.plot(result)
+plt.xlabel("Time Period")
+plt.ylabel("Stock Price")
+plt.show()
